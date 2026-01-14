60c952ded542d6254d3251fb0239c21ef1109097
spi9325 <sanketinamdar72@gmail.com>
2026-01-14 20:15:58 +0530

    optimize function call TOOLS[]

--- apps/backend/src/agent1.js
+++ apps/backend/src/agent1.js
@@ -6,7 +6,7 @@ const ai = new GoogleGenAI({
     apiKey: GENAIKEY
 });
 
-function sum({num1,num2}) {
+function sum({ num1, num2 }) {
     return num1 + num2
 }
 
@@ -30,30 +30,35 @@ const declareSumFn = {
 }
 
 async function main() {
-        const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
-        contents: [{role:'user',parts:[{text:'what is sum of 1000 and 100'}]}],
-        config: {
-            tools: [{
-                functionDeclarations: [declareSumFn]
-            }],
-        }
-    });
-    if (response.functionCalls && response.functionCalls.length > 0) {
-        const functionCall = response.functionCalls[0]; // Assuming one function call
-        console.log(`Function to call: ${functionCall.name}`);
-        console.log(`Arguments: ${JSON.stringify(functionCall.args)}`);
-        // In a real app, you would call your actual function here:
-        // const result = await getCurrentTemperature(functionCall.args);
-        if(functionCall.name == 'sum'){
-            const res= sum(functionCall.args)
-            console.log(res,"================")
-        }
-    } else {
-        console.log("No function call found in the response.");
-        console.log(response.text);
-        
+    const TOOLS = {
+        sum,
     }
-    }
+    const res = TOOLS['sum']
+    const r= res({num1:1,num2:3})
+    console.log(r)
+    // const response = await ai.models.generateContent({
+    //     model: "gemini-3-flash-preview",
+    //     contents: [{ role: 'user', parts: [{ text: 'what is sum of 1000 and 100' }] }],
+    //     config: {
+    //         tools: [{
+    //             functionDeclarations: [declareSumFn]
+    //         }],
+    //     }
+    // });
+    // if (response.functionCalls && response.functionCalls.length > 0) {
+    //     const functionCall = response.functionCalls[0]; // Assuming one function call
+    //     console.log(`Function to call: ${functionCall.name}`);
+    //     console.log(`Arguments: ${JSON.stringify(functionCall.args)}`);
+    //     // In a real app, you would call your actual function here:
+    //     // const result = await getCurrentTemperature(functionCall.args);
+    //     const Called = TOOLS[functionCall.name]
+    //     const result  = Called(functionCall.args)
+    //     console.log(result)
+    // } else {
+    //     console.log("No function call found in the response.");
+    //     console.log(response.text);
+
+    // }
+}
 
 main();
